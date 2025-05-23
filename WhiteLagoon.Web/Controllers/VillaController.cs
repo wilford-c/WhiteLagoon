2b3db20d84467b710134603efd3a1309af547b7b
Wilford Chidawanyika <wilford.chidawanyika.com>
2025-05-23 10:58:59 +0200

    Added a view and retrieved data from the database

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -14,7 +14,7 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Index()
         {
             var villas = _db.Villas.ToList();
-            return View();
+            return View(villas);
         }
     }
 }
